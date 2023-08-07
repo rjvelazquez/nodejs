9be13bf946d72f3a45873dd433beea3fc5a465e5
Roberto Velazquez <rochugui10@gmail.com>
2023-08-06 21:30:40 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const http = require('http');
 
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+  console.log(`Holaaaaa`);
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
